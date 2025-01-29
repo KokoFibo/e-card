2831a8701c28a0ff84ee1c4e197e600b0b23cad7
KokoFibo <kokonacci@gmail.com>
2025-01-29 22:56:25 +0700

    new API 5

--- routes/web.php
+++ routes/web.php
@@ -30,8 +30,8 @@ Route::middleware('auth')->group(function () {
 
     Route::post('/client/store', [ClientController::class, 'store']);
 
-    Route::get('/profile/edit', [ClientController::class, 'edit'])->name('client.edit');
-    Route::post('/profile/update', [ClientController::class, 'update'])->name('client.update');
+    Route::get('/client/edit', [ClientController::class, 'edit'])->name('client.edit');
+    Route::post('/client/update', [ClientController::class, 'update'])->name('client.update');
 });
 
 require __DIR__ . '/auth.php';
